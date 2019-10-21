8359c5ba5c389fe14fc802bb3d6e71e6331ded11
alexsegui <36410045+alexsegui@users.noreply.github.com>
2019-10-21 13:26:07 +0200

    suprimir menus a pagina acatar

--- crea-usu.sql
+++ crea-usu.sql
@@ -1,1 +1,1 @@
-create table usuaris (nom varchar(15) not null, passw varchar(30) not null, ninot tinyint primary key, grup tinyint);
+create table usuaris (nom varchar(15) not null, passw varchar(30) not null, ninot tinyint primary key, grup vsrchsr(3));
