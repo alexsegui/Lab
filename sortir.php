cc40bc955f82e535b16bcd9b975edc1339bee663
alexsegui <36410045+alexsegui@users.noreply.github.com>
2019-10-21 16:50:26 +0200

    enllaç --sortir --
    Enllaç que esborra totes les variables de sessió i et situa a index.html

---
+++ sortir.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!--
+Template Name: Abele
+Author: <a href="https://www.os-templates.com/">OS Templates</a>
+Author URI: https://www.os-templates.com/
+Licence: Free to use under our free template licence terms
+Licence URI: https://www.os-templates.com/template-terms
+--> 
+<html>
+<head>
+
+
+<?php session_start(); ?>
+    <title>LABORATORI</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body id="top">   
+    
+<?php
+
+// Destruir totes les variables de sesió.
+$_SESSION = array();
+
+
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+//  destruir la sesió.
+session_destroy();
+header ("Location: /index.html");	
+?>
+
+</body>
+</html>
